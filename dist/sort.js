2bf7c7e5d5030d96c21216ef4d7c8e735728f81e
SarawutCoding <sarawutphuangsumalee@gmail.com>
2025-05-19 13:31:31 +0700

    first commit

---
+++ dist/sort.js
@@ -0,0 +1,20 @@
+"use strict";
+let num = [5, 6, 1, 9, 8, 4, 4, 5, 6, 8];
+function grade(numtest) {
+    let numSum = [];
+    let rever;
+    for (let i = 0; i < numtest.length - 1; i++) {
+        for (let j = 0; j < numtest.length - 1; j++) {
+            if (numtest[j] > numtest[j + 1]) {
+                rever = numtest[j + 1];
+                numtest[j + 1] = numtest[j];
+                numtest[j] = rever;
+            }
+            else {
+                numSum = numtest;
+            }
+        }
+    }
+    return numSum;
+}
+console.log(grade(num));
---
+++ src/sort.ts
@@ -0,0 +1,19 @@
+let num: number[] = [5, 6, 1, 9, 8, 4, 4, 5 , 6 , 8];
+
+function grade(numtest:number[]) {
+    let numSum: number[] = []
+    let rever:number
+    for (let i = 0; i < numtest.length - 1; i++) {
+        for (let j = 0; j < numtest.length - 1; j++) {
+            if (numtest[j] > numtest[j + 1]) {
+                rever = numtest[j + 1];
+                numtest[j + 1] = numtest[j];
+                numtest[j] = rever;
+            } else {
+                numSum = numtest;
+            }
+        }
+    }
+    return numSum;
+}
+console.log(grade(num));
